04a22166e75409a7fc084baf988bed4551274a9e
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2022-10-10 14:10:12 +0100

    Build and run on Joe's mac, and fix server start bug

--- helloWorld/src/main/java/com/acme/hello/HelloImpl.java
+++ helloWorld/src/main/java/com/acme/hello/HelloImpl.java
@@ -1,18 +1,5 @@
 package com.acme.hello;
 
-import java.rmi.Naming;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-
-
-
-public class HelloImpl implements Hello
-{
-    public HelloImpl() throws RemoteException {
-    }
-
+public class HelloImpl implements Hello {
     public String sayHello() { return "Hello world!"; }
-
 }
